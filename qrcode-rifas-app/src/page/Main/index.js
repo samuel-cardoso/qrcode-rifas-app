cebd90fca3ba59b56a0086c8014678689af0b49d
Samuel Cardoso da Silva <samuelcardoso564@gmail.com>
2023-05-17 00:18:35 -0300

    finalizando projeto

--- qrcode-rifas-app/src/page/Main/index.js
+++ qrcode-rifas-app/src/page/Main/index.js
@@ -9,12 +9,15 @@ import 'primeicons/primeicons.css';
 import QRCode from "react-qr-code";
 import QRCodeLink from "qrcode";
 
-function Main() {
+import logo from './img/logo-qrcoderifas.png'
 
-    const searchParams = new URLSearchParams(document.location.search)
 
-    const [link, setLink] = useState(searchParams.get('samuel') || '');
+export default function Main() {
+    const searchParams = new URLSearchParams(document.location.search);
+
+    const [link, setLink] = useState(searchParams.get('link') || '');
     const [qrcodeLink, setQrcodeLink] = useState('');
+    const [isValidUrl, setIsValidUrl] = useState(true);
 
     function handleGenerate(link_url) {
         QRCodeLink.toDataURL(link_url, {
@@ -22,43 +25,67 @@ function Main() {
             margin: 3,
         }, function (err, url) {
             setQrcodeLink(url);
-        })
+        });
     }
 
     function handleQrcode(e) {
-        setLink(e.target.value);
-        handleGenerate(e.target.value);
+        const inputUrl = e.target.value;
+        setLink(inputUrl);
+        setIsValidUrl(validateUrl(inputUrl));
+        if (isValidUrl) {
+            handleGenerate(inputUrl);
+        }
+    }
+
+    function validateUrl(url) {
+        const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+        return url.match(urlRegex);
+    }
+
+    function handleDownload(e) {
+        if (!isValidUrl) {
+            e.preventDefault();
+        }
     }
 
     return (
         <div className="App">
-
-            <main className="main min-h-screen px-5 py-5 grid grid-cols-2 items-center drop-shadow-xl">
-
-                <div className="description">
-                    <h1>Área para Descrição</h1>
-                </div>
-
-                <div>
-
-                    <div className="card flex flex-col justify-content-center mb-10">
-                        <QRCode className="hidden" value={link} />
-                        <h1 className="title-input text-3xl font-bold mb-5">Insira o link da sua rifa</h1>
-                        <InputText type="URL" placeholder="https://www.suarifa.com.br" value={link} onChange={(e) => handleQrcode(e)} />
+            <main className="min-h-screen flex items-center justify-center bg-gray-100">
+                <div className="max-w-4xl mx-auto px-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                        <div className="flex flex-col justify-center">
+                            <img src={logo} className="w-60 mb-10 mx-auto" alt="Logo" />
+                            <h1 className="text-4xl md:text-5xlxl text-center leading-tight">
+                                Gere em poucos segundos um <span>QR Code</span> para sua Rifa!
+                            </h1>
+                        </div>
+                        <div className="flex flex-col justify-center">
+                            <div className="card bg-white px-12 py-20 rounded-md shadow-md">
+                                <QRCode className="hidden" value={link} />
+                                <h1 className="text-2xl md:text-2xl font-bold mb-5">
+                                    Insira o link da sua rifa
+                                </h1>
+                                <div className="mb-4">
+                                    <InputText
+                                        type="URL"
+                                        placeholder="https://www.suarifa.com.br"
+                                        value={link}
+                                        onChange={handleQrcode}
+                                        className={!isValidUrl ? "p-invalid" : ""}
+                                    />
+                                </div>
+                                {!isValidUrl && <small className="text-red-500">Por favor, digite uma URL válida.</small>}
+                                <div className="flex flex-wrap justify-center gap-3 mt-7">
+                                    <a className="w-full" href={qrcodeLink} download={isValidUrl ? "qrcode.png" : ""} onClick={handleDownload}>
+                                        <Button className="rounded-full" label="Gerar QR Code" disabled={!isValidUrl} />
+                                    </a>
+                                </div>
+                            </
+                            div>
+                        </div>
                     </div>
-
-
-                    <div className="card flex flex-wrap justify-content-center gap-3 d-none">
-                        
-                        <a href={qrcodeLink} download={"qrcode.png"}>
-                        <Button label="Gerar QR Code"/>
-                        </a>
-                    </div>
                 </div>
-
             </main>
         </div>
     );
 }
-
-export default Main;
